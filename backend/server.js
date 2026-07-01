9878888e74424541186fb249935568e1264a80b8
LJay18 <sowunmilekan7@gmail.com>
2026-07-01 16:17:04 +0100

    Use Render port

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,5 @@ res.json({ message: "Attendance updated" });
 });
 
 app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+    console.log(`Server running on port ${PORT}`);
 });
